a3c85956a4fcf07886c287c4a56955ecc93c3293
Ben Wellnitz <106043193+BenWellnitz@users.noreply.github.com>
2023-01-20 13:07:01 -0500

    Delete TiffReadRM.m

--- fcsSOFI_external_functions/TiffReadRM.m
+++
@@ -1,9 +0,0 @@
-function [Data1] = TiffReadRM(filekey, filepath, startframe, endframe)
-    fname = strcat(filepath, filekey);
-    i = 1; %counter
-    for j = startframe:endframe
-        Data1(:, :, i) = imread(fname, j);
-        i = i + 1;
-    end
-    return
-end
