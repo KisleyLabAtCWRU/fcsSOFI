1605dd401c53a2a676e9f0c603c9b89079f21086
Ben Wellnitz <106043193+BenWellnitz@users.noreply.github.com>
2023-04-04 11:44:33 -0400

    Fixed Cross Decon Issue
    No longer applies sat max in this function. Applies satMax at the very end of the analysis.

--- fcsSOFI_external_functions/crossSofi.m
+++ fcsSOFI_external_functions/crossSofi.m
@@ -1,9 +1,7 @@
-function [crossSofiVirt, crossSofi] = crossSofi(data, doNormalCross, satMax)
+function [crossSofi] = crossSofi(data, doNormalCross)
 
 
     %% Variable Set Up
-    crossSofi = 0;
-    crossSofiVirt = 0;
     sz = size(data);
     height = sz(1);
     width = sz(2);
@@ -11,9 +9,7 @@ function [crossSofiVirt, crossSofi] = crossSofi(data, doNormalCross, satMax)
 
 
     %% Cross Correlation Calculations
-
-    % If no virtual pixels wanted
-    if doNormalCross
+    if doNormalCross % Normal Cross Correlation
         sofi = zeros(height, width);
         for i = 2:height-1
             for j = 2:width-1
@@ -31,29 +27,30 @@ function [crossSofiVirt, crossSofi] = crossSofi(data, doNormalCross, satMax)
                 disp('Sofi No Virtual Cross Corr 100% Done')
             end
         end
-    
-    % Else do have virtual pixels
-    else
-        vSofi = zeros(height*2-1, width*2-1);
+        sofi = sofi(2:height-2, 2:width-2);
+
+
+    else % Virtual Cross Correlation
+        sofi = zeros(height*2-1, width*2-1);
         for i = 2:height*2-2
             for j = 2:width*2-2
                 if (mod(i,2) == 0)
                     if (mod(j,2) == 0) % i even, j even
                         downDiag = xcorr(data(:, convert(i-1), convert(j-1)), data(:, convert(i+1), convert(j+1)), 0);
                         upDiag = xcorr(data(:, convert(i-1), convert(j+1)), data(:, convert(i+1), convert(j-1)), 0);
-                        vSofi(i, j) = ((downDiag + upDiag) / 2);
+                        sofi(i, j) = ((downDiag + upDiag) / 2);
                     else % i even, j odd
                         aboveBelow = xcorr(data(:, convert(i-1), convert(j)), data(:, convert(i+1), convert(j)), 0);
-                        vSofi(i, j) = aboveBelow;
+                        sofi(i, j) = aboveBelow;
                     end
                 else
                     if (mod(j,2) == 0) % i odd, j even
                         leftRight = xcorr(data(:, convert(i), convert(j-1)), data(:, convert(i), convert(j+1)), 0);
-                        vSofi(i, j) = leftRight;
+                        sofi(i, j) = leftRight;
                     else % i odd, j odd
                         aboveBelow = xcorr(data(:, convert(i-2), convert(j)), data(:, convert(i+2), convert(j)), 0);
                         leftRight = xcorr(data(:, convert(i), convert(j-2)), data(:, convert(i), convert(j+2)), 0);
-                        vSofi(i, j) = ((aboveBelow + leftRight) / 2);
+                        sofi(i, j) = ((aboveBelow + leftRight) / 2);
                     end
                 end
             end
@@ -66,17 +63,18 @@ function [crossSofiVirt, crossSofi] = crossSofi(data, doNormalCross, satMax)
             elseif i == round(height * 2 - 2)
                 disp('Sofi Cross Corr 100% Done')
             end
+
         end
-    
+
         %% Virtual Pixel Corrections
-        horizPlusVirtMean = mean(reshape(vSofi(3:2:height*2-2, 3:2:width*2-2), 1, []));
-        horizPlusVirtStd = std(reshape(vSofi(3:2:height*2-2, 3:2:width*2-2), 1, []));
-        horizonMean = mean(reshape(vSofi(3:2:height*2-2, 2:2:width*2-2), 1, []));
-        horizonStd = std(reshape(vSofi(3:2:height*2-2, 2:2:width*2-2), 1, []));
-        virtMean = mean(reshape(vSofi(2:2:height*2-2, 3:2:width*2-2), 1, []));
-        virtStd = std(reshape(vSofi(2:2:height*2-2, 3:2:width*2-2), 1, []));
-        diagMean = mean(reshape(vSofi(2:2:height*2-2, 2:2:width*2-2), 1, []));
-        diagStd = std(reshape(vSofi(2:2:height*2-2, 2:2:width*2-2), 1, []));
+        horizPlusVirtMean = mean(reshape(sofi(3:2:height*2-2, 3:2:width*2-2), 1, []));
+        horizPlusVirtStd = std(reshape(sofi(3:2:height*2-2, 3:2:width*2-2), 1, []));
+        horizonMean = mean(reshape(sofi(3:2:height*2-2, 2:2:width*2-2), 1, []));
+        horizonStd = std(reshape(sofi(3:2:height*2-2, 2:2:width*2-2), 1, []));
+        virtMean = mean(reshape(sofi(2:2:height*2-2, 3:2:width*2-2), 1, []));
+        virtStd = std(reshape(sofi(2:2:height*2-2, 3:2:width*2-2), 1, []));
+        diagMean = mean(reshape(sofi(2:2:height*2-2, 2:2:width*2-2), 1, []));
+        diagStd = std(reshape(sofi(2:2:height*2-2, 2:2:width*2-2), 1, []));
     
         aHorizon = horizPlusVirtStd / horizonStd;
         bHorizon = horizPlusVirtMean - aHorizon*horizonMean;
@@ -85,34 +83,14 @@ function [crossSofiVirt, crossSofi] = crossSofi(data, doNormalCross, satMax)
         aDiag = horizPlusVirtStd / diagStd;
         bDiag = horizPlusVirtMean - aDiag*diagMean;
     
-        vSofi(3:2:height*2-2, 2:2:width*2-2) = aHorizon * vSofi(3:2:height*2-2, 2:2:width*2-2) + bHorizon;
-        vSofi(2:2:height*2-2, 3:2:width*2-2) = aVert * vSofi(2:2:height*2-2, 3:2:width*2-2) + bVert;
-        vSofi(2:2:height*2-2, 2:2:width*2-2) = aDiag * vSofi(2:2:height*2-2, 2:2:width*2-2) + bDiag;
+        sofi(3:2:height*2-2, 2:2:width*2-2) = aHorizon * sofi(3:2:height*2-2, 2:2:width*2-2) + bHorizon;
+        sofi(2:2:height*2-2, 3:2:width*2-2) = aVert * sofi(2:2:height*2-2, 3:2:width*2-2) + bVert;
+        sofi(2:2:height*2-2, 2:2:width*2-2) = aDiag * sofi(2:2:height*2-2, 2:2:width*2-2) + bDiag;
+
+        sofi = sofi(2:height*2-2, 2:width*2-2); 
     end
 
-
-    %% Image Normailzation and Creation
-    if doNormalCross
-        sofi = sofi(2:height-2, 2:width-2);
-        sofi = sofi - (min(min(sofi)));
-        sofi = sofi ./ (max(max(sofi)));
-
-        sofi(sofi > satMax) = satMax;
-        sofi = sofi ./ (max(max(sofi)));
-        
-        crossSofi = sofi;
-    else
-
-    vSofi = vSofi(2:height*2-2, 2:width*2-2);
-    vSofi = vSofi - (min(min(vSofi)));
-    vSofi = vSofi ./ (max(max(vSofi)));
-
-    vSofi(vSofi > satMax) = satMax;
-    vSofi = vSofi ./ (max(max(vSofi)));
-
-    crossSofiVirt = vSofi;
-    end
-
+    crossSofi = sofi;
 end
 
 % Converts a virtual index into a real index
